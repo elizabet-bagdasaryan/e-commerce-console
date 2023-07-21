61d0b1b5b1c7783d70ef2136638078fbd0ef2711
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-21 14:50:59 +0400

    feat:products adding functions

---
+++ src/product.js
@@ -0,0 +1,23 @@
+let products = []
+
+function findProdIndex(productId){
+    return products.findIndex((product) => product.id === productId)
+}
+
+export default function getProdById(productId){
+    return products.find((product)=> product.id === productId)
+}
+
+export default function saveProduct(name, price,id){
+    const existProdIndex = findProdIndex(id)
+
+    if (existProdIndex !== -1){
+        products[existProdIndex].name = name
+        products[existProdIndex].price = price
+    }
+    //tu arsebobs vaaupdatebt
+ else {
+    products.push({name,price,id})
+}
+
+}
