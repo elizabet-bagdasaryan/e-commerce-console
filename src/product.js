1df5d82c75d8acc4af99986857f37760e790af09
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-21 15:37:30 +0400

    feat: add cart options functions

--- src/product.js
+++ src/product.js
@@ -1,23 +1,22 @@
-let products = []
+let products = [];
 
-function findProdIndex(productId){
-    return products.findIndex((product) => product.id === productId)
+function findProdIndex(productId) {
+  return products.findIndex((product) => product.id === productId);
 }
 
-export default function getProdById(productId){
-    return products.find((product)=> product.id === productId)
+export function getProdById(productId) {
+  return products.find((product) => product.id === productId);
 }
 
-export default function saveProduct(name, price,id){
-    const existProdIndex = findProdIndex(id)
+export function saveProduct(name, price, id) {
+  const existProdIndex = findProdIndex(id);
 
-    if (existProdIndex !== -1){
-        products[existProdIndex].name = name
-        products[existProdIndex].price = price
-    }
-    //tu arsebobs vaaupdatebt
- else {
-    products.push({name,price,id})
-}
-
+  if (existProdIndex !== -1) {
+    products[existProdIndex].name = name;
+    products[existProdIndex].price = price;
+  }
+  //tu arsebobs vaaupdatebt
+  else {
+    products.push({ name, price, id });
+  }
 }
